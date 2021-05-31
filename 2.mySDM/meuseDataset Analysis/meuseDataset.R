ec57f4cf9bc2384fd8bbd7a91bedfe9c817828bc
okomojacob <jaylabs2030@gmail.com>
2021-05-31 23:53:45 +0300

    predict zinc conc... plots :bug: fixed

--- 2.mySDM/meuseDataset Analysis/meuseDataset.R
+++ 2.mySDM/meuseDataset Analysis/meuseDataset.R
@@ -3,7 +3,8 @@ library(sp)
 library(lattice)
 
 # call the meuse data
-data("meuse")
-coordinates(meuse) <- c("x", "Y")
+data(meuse)
+coordinates(meuse) <- c("x", "y")
 
 # Predict topsoil zinc concentration
+spplot(meuse, "zinc", do.log = T, colorkey = TRUE)
