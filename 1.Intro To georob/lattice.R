74727b5f8d6806d8c1f31d0b9b30452dbe997266
okomojacob <jaylabs2030@gmail.com>
2021-05-31 23:04:12 +0300

    :pencil: :hammer: fixed :bug:

--- 1.Intro To georob/lattice.R
+++ 1.Intro To georob/lattice.R
@@ -1,12 +1,14 @@
 library(sp)
+library(lattice)
 data(meuse, package="sp")
 levels(meuse$ffreq) <- paste("ffreq", levels(meuse$ffreq), sep="")
 levels(meuse$soil) <- paste("soil", levels(meuse$soil), sep="")
 str(meuse)
-library(lattice)
+
 palette(trellis.par.get("superpose.symbol")$col)
 plot(zinc~dist, meuse, pch=as.integer(ffreq), col=soil)
 
+# Add lattice legend at the topright
 legend("topright", col=c(rep(1, nlevels(meuse$ffreq)), 1:nlevels(meuse$soil)),
        pch=c(1:nlevels(meuse$ffreq), rep(1, nlevels(meuse$soil))), bty="n",
        legend=c(levels(meuse$ffreq), levels(meuse$soil)))
