6882cfae554a6d3fae469b0f884b0b113007dc7a
okomojacob <jaylabs2030@gmail.com>
2021-06-06 15:23:25 +0300

    :bug:, Central Tendency :hammer: :pencil:

--- 7.Intro To Geostats/1.ESDA/ESDA_CentralTendency.R
+++ 7.Intro To Geostats/1.ESDA/ESDA_CentralTendency.R
@@ -1,19 +1,5 @@
 #Measures of Cental Tendency
 
-#EDA to check the trend, normality, deviations: Histograms, boxplots,measure of central tendency, dispersion
-rm(list = ls(all=TRUE)) #clear memory
-
-#Load existing/install missing pckges, use :: to access a specific fxn from a package
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(gstat, sp)
-data(meuse)
-df <- meuse
-knitr::kable(head(df, n=5), align = 'l')
-
-# outlier Detection
-##-> boxplot.stats()$cut, to identify and remove data 1.5<x>1.5 , where x is the IQR
-# Meuse Data
-
 rm(list=ls(all=TRUE)) # Clear memory
 #Load existing/install missing libraries
 if (!require("pacman")) install.packages("pacman")
@@ -29,3 +15,21 @@ cat("The mean is ", vMean)
 # 2. Median of Copper metal
 vMedian <- median(df$copper)
 cat("The median is ", vMedian)
+
+#3. Mode
+library(modeest)
+vMode <- getmode(copper)
+cat("The mode is", vMode)
+
+#4. Measures of Dispersion
+
+
+
+
+
+
+
+
+
+
+
