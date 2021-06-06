15cdb2ea13a694f9eddf5c598f39bcff659186dc
okomojacob <jaylabs2030@gmail.com>
2021-06-06 17:12:11 +0300

    :bug: Normal Distributions :hammer: :pencil:

---
+++ 7.Intro To Geostats/1.ESDA/normalDistribution.R
@@ -0,0 +1,17 @@
+# Normal Distribution
+
+x<-rnorm(100000,mean=10, sd=2) 
+hist(x,breaks=150,xlim=c(0,20),freq=FALSE)
+abline(v=10, lwd=5)
+abline(v=c(4,6,8,12,14,16), lwd=3,lty=3)
+
+# Create a sequence of numbers between -10 and 10 incrementing by 0.2.
+x <- seq(-10,10,by = .2)
+# Choose the mean as 2.5 and standard deviation as 2. 
+y <- pnorm(x, mean = 2.5, sd = 2)
+# Plot the graph.
+plot(x,y)
+
+
+qqnorm(copper, main='Copper in Meuse River')
+qqline(copper)
