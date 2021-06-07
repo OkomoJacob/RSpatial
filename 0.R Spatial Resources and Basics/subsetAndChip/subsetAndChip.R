efe7a3404aae874cbc76befe11e7136137cb962d
okomojacob <jaylabs2030@gmail.com>
2021-06-07 15:46:00 +0300

    :bug: Clipping :hammer:

--- 0.R Spatial Resources and Basics/subsetAndChip/subsetAndChip.R
+++ 0.R Spatial Resources and Basics/subsetAndChip/subsetAndChip.R
@@ -4,7 +4,9 @@ install.packages(c("raster", "rgdal"))
 # Import the packages for raster img analysis
 library(raster)     #raster
 library(rgdal)      #vector
-setwd("D:/STUDY/4.GIS/G I S 4.1/2.Geostats/0x520x/INPUT/L8/168061-L82019")
+library(stars)
+setwd("D:/STUDY/4.GIS/G I S 4.1/2.Geostats/0x520x/INPUT/L8/L8_Ngara_TZ_Ag_2020")
 getwd()
+
 #Read and plot data (L8 Data)
 
