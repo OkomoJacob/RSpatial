ac55a689cbc4ed09d7d105733fd0287e6253d7f2
okomojacob <jaylabs2030@gmail.com>
2021-07-11 17:35:41 +0300

    Updates, Pracs

--- 6.Meuse Dataset Spatial Analysis/linearModellingMeuse.R
+++ 6.Meuse Dataset Spatial Analysis/linearModellingMeuse.R
@@ -1,14 +1,129 @@
-# Import the 2 packages
+# Lineaar Modelling of spatila Data  to explain the distribution of zinc concentration within the site.
+# Clear memory and load aster data packages
+rm(list = ls(all=TRUE))
 library(sp)
 library(lattice)
 library(ggplot2)
 library(scales)
 library(gstat)
+library(sf)
 
-# call the meuse data
+data(meuse)## loading data
+meuse = st_as_sf(meuse,coords=c("x","y"),remove=FALSE) ## transforming data frame into sf object
+# Descriptive statistics of the meuse dataset
+summary(meuse)
+
+# View the spatial dataset on Meuse Map
+#1. Loading gridded data
+data(meuse.grid) 
+coordinates(meuse.grid) = c("x", "y")
+
+#2. Transform gridded data into Spatial Pixels
+meuse.grid = as(meuse.grid, "SpatialPixels") 
+
+#3. loading Meuse river data
+data(meuse.riv) 
+meuse.lst = list(Polygons(list(Polygon(meuse.riv)),"meuse.riv"))
+
+#4. concatenate all polygons of the river to reconstruct it entirely
+meuse.sr = SpatialPolygons(meuse.lst) 
+image(meuse.grid, col = "yellowgreen")
+
+#5. plot the river data
+plot(meuse.sr, col = "grey", add = TRUE)
+
+#6. plot the zinc dataset
+plot(meuse[,c('zinc')], add = TRUE, cex=1.5)
+#7. Add title
+title("Meuse dataset")
+
+
+
+
+
+
+
+
+
+#create a categorical variable
+meuse$zinc_cat <-cut(meuse$zinc, breaks = c(0,200,400,800,1200,2000))
+zinc.plot <- ggplot(aes(x=x, y=y), data = meuse)
+
+zinc.plot<-zinc.plot+geom_point(aes(color = zinc_cat))
+
+zinc.plot <- zinc.plot+coord_equal()
+
+zinc.plot <- zinc.plot+scale_color_brewer(pallete = "YlGnBu")
+# Spatial distribution of Zinc
+zinc.plot
+
+# Predict topsoil zinc concentration using spplot & bubble plots
+spplot(meuse, "zinc", do.log = T, colorkey = TRUE)
+bubble(meuse, "zinc", do.log = T, key.space = "right")
+
+data("meuse.grid")
+coordinates(meuse.grid) <- c("x", "y")
+meuse.grid <- as(meuse.grid, "SpatialGridDataFrame")
+
+
+# Import the gstat pkg to perform the IDW
+idw.out <- idw(zinc~1, meuse, meuse.grid, idp=2)
+
+## inverse distance weighted interpolation
+as.data.frame(idw.out)[1:5,]
+
+# Linear regression Models for Predictions
+xyplot(log(zinc) ~ sqrt(dist), as.data.frame(meuse))
+
+# Fitting the Linear Models
+zn.lm <- lm(log(zinc) ~ sqrt(dist), meuse)
+meuse$fitted.s <- predict(zn.lm,meuse) - mean(predict(zn.lm,meuse))
+meuse$residuals <- residuals(zn.lm)
+spplot(meuse, c("fitted.s", "residuals"))
+
+## Estimate spatial correlation: variogram
+hscat(log(zinc)~1,meuse,(0:9)*100)
+
+#Experimental Variogram
+plot(variogram(log(zinc)~1,meuse),type="h")
+
+# Randomly allocate the zinc concentration to different locations and see the relatively flat experimental variogram, showing no correlations.
+data <- meuse
+n <- nrow(meuse)
+set.seed(15)
+ind <- sample(1:n, size=n, replace = F)
+data$zinc <- meuse$zinc[ind]
+plot(variogram(log(zinc)~1, data), type="l")
+
+
+#---------------------------------------------
+# Import the geostats libraries
+# library(sp)
+library(lattice) #for plotting graphics
+library(sp)
+#call the meuse R data
 data(meuse)
 
+#ffreq = flooding frequency
 
+levels(meuse$ffreq) <- paste("ffreq", levels(meuse$ffreq), sep="")
+levels(meuse$soil) <- paste("soil", levels(meuse$soil), sep="")
+str(meuse)
+
+palette(trellis.par.get("superpose.symbol")$col)
+plot(zinc~dist, meuse, pch=as.integer(ffreq), col=soil)
+
+# Add lattice legend at the topright
+legend("topright", col=c(rep(1, nlevels(meuse$ffreq)), 1:nlevels(meuse$soil)),
+       pch=c(1:nlevels(meuse$ffreq), rep(1, nlevels(meuse$soil))), bty="n",
+       legend=c(levels(meuse$ffreq), levels(meuse$soil)))
+
+xyplot(log(zinc)~dist | ffreq, meuse, groups=soil, panel=function(x, y, ...)
+{
+  panel.xyplot(x, y, ...)
+  panel.loess(x, y, ...)
+}, 
+auto.key=TRUE)
 
 
 
--- 6.Meuse Dataset Spatial Analysis/meuseDataset.R
+++ 6.Meuse Dataset Spatial Analysis/meuseDataset.R
@@ -23,7 +23,25 @@ zinc.plot <- zinc.plot+scale_color_brewer(pallete = "YlGnBu")
 
 zinc.plot
 
+#ffreq = flooding frequency
+levels(meuse$ffreq) <- paste("ffreq", levels(meuse$ffreq), sep="")
+levels(meuse$soil) <- paste("soil", levels(meuse$soil), sep="")
+str(meuse)
 
+palette(trellis.par.get("superpose.symbol")$col)
+plot(zinc~dist, meuse, pch=as.integer(ffreq), col=soil)
+
+# Add lattice legend at the topright
+legend("topright", col=c(rep(1, nlevels(meuse$ffreq)), 1:nlevels(meuse$soil)),
+       pch=c(1:nlevels(meuse$ffreq), rep(1, nlevels(meuse$soil))), bty="n",
+       legend=c(levels(meuse$ffreq), levels(meuse$soil)))
+
+xyplot(log(zinc)~dist | ffreq, meuse, groups=soil, panel=function(x, y, ...)
+{
+  panel.xyplot(x, y, ...)
+  panel.loess(x, y, ...)
+}, 
+auto.key=TRUE)
 
 
 
