e47a34a0c3292579acbd20f22e31497caa47a99b
okomojacob <jaylabs2030@gmail.com>
2021-07-11 20:33:02 +0300

    R square, :bug:, fixes :pen:

--- 6.Meuse Dataset Spatial Analysis/linearModellingMeuse.R
+++ 6.Meuse Dataset Spatial Analysis/linearModellingMeuse.R
@@ -58,9 +58,11 @@ st_geometry(meuse) = NULL
 #2. Print correlations between quantitative variables, 
 ggpairs(meuse[,3:9])
 
-## We can see that Zinc has the highest R2 values, therefore we go for it in this spatial modelling
-# Create a new grid function to plot the zinc statistics individually, : zinc, 
+## We can see that Zinc has the highest R2 values, therefore we go for it in this spatial modeling
 
+# --------------- STEP 5 SPREAD, LOG Transform, Representation of VARIATION---------------- #
+# Create a new grid function to plot the zinc statistics individually, : zinc, 
+# Relationships between zinc concentration and the qualitative variables flood frequency, soil type, lime class and LULCC
 #3. Plot zinc against elevation and log zinc against elev
 plot_grid(
   ggplot(meuse, aes(y = zinc, x = elev)) + geom_point(),
@@ -74,7 +76,7 @@ plot_grid(
   ggplot(meuse, aes(y = log(zinc), x = sqrt(dist))) + geom_point(),
   nrow = 1, ncol = 3)
 
-#3. Plot representation of variation using the 
+#3. Plot representation of variation using the Boxplot
 plot_grid(
   ggplot(meuse, aes(y = log(zinc), x = ffreq)) + geom_boxplot(),
   ggplot(meuse, aes(y = log(zinc), x = soil)) + geom_boxplot(),
@@ -82,75 +84,13 @@ plot_grid(
   ggplot(meuse, aes(y = log(zinc), x = landuse)) + geom_boxplot(),
   nrow = 2, ncol = 2)
 
+# --------------- STEP 6 LINEAR MODELLING-------------
 
-
-
-
-
-
-
-
-
-
-#create a categorical variable
-meuse$zinc_cat <-cut(meuse$zinc, breaks = c(0,200,400,800,1200,2000))
-zinc.plot <- ggplot(aes(x=x, y=y), data = meuse)
-
-zinc.plot<-zinc.plot+geom_point(aes(color = zinc_cat))
-
-zinc.plot <- zinc.plot+coord_equal()
-
-zinc.plot <- zinc.plot+scale_color_brewer(pallete = "YlGnBu")
-# Spatial distribution of Zinc
-zinc.plot
-
-# Predict topsoil zinc concentration using spplot & bubble plots
-spplot(meuse, "zinc", do.log = T, colorkey = TRUE)
-bubble(meuse, "zinc", do.log = T, key.space = "right")
-
-data("meuse.grid")
-coordinates(meuse.grid) <- c("x", "y")
-meuse.grid <- as(meuse.grid, "SpatialGridDataFrame")
-
-
-# Import the gstat pkg to perform the IDW
-idw.out <- idw(zinc~1, meuse, meuse.grid, idp=2)
-
-## inverse distance weighted interpolation
-as.data.frame(idw.out)[1:5,]
-
-# Linear regression Models for Predictions
-xyplot(log(zinc) ~ sqrt(dist), as.data.frame(meuse))
-
-# Fitting the Linear Models
-zn.lm <- lm(log(zinc) ~ sqrt(dist), meuse)
-meuse$fitted.s <- predict(zn.lm,meuse) - mean(predict(zn.lm,meuse))
-meuse$residuals <- residuals(zn.lm)
-spplot(meuse, c("fitted.s", "residuals"))
-
-## Estimate spatial correlation: variogram
-hscat(log(zinc)~1,meuse,(0:9)*100)
-
-#Experimental Variogram
-plot(variogram(log(zinc)~1,meuse),type="h")
+#3 quantitative variables (elevation over thesite, squared distance to the river, organic matter content) AND
+#3 qualitative variables (frequency of flooding, soil type, and presence or absence of limestone) affect the zinc concentration on the study plain.
 
 # Randomly allocate the zinc concentration to different locations and see the relatively flat experimental variogram, showing no correlations.
-data <- meuse
-n <- nrow(meuse)
-set.seed(15)
-ind <- sample(1:n, size=n, replace = F)
-data$zinc <- meuse$zinc[ind]
-plot(variogram(log(zinc)~1, data), type="l")
-
-
-#---------------------------------------------
-# Import the geostats libraries
-library(sp)
-#call the meuse R data
-data(meuse)
-
-#ffreq = flooding frequency
-
+# Model how IDW affects the distance
 levels(meuse$ffreq) <- paste("ffreq", levels(meuse$ffreq), sep="")
 levels(meuse$soil) <- paste("soil", levels(meuse$soil), sep="")
 str(meuse)
@@ -169,17 +109,3 @@ xyplot(log(zinc)~dist | ffreq, meuse, groups=soil, panel=function(x, y, ...)
   panel.loess(x, y, ...)
 }, 
 auto.key=TRUE)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
